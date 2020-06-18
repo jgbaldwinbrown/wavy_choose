27fd26d847dfb80568a07eec3fcdc10b50c6361c
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-06-17 18:08:59 -0600

    updated testall

--- testing/testall.sh
+++ testing/testall.sh
@@ -5,5 +5,7 @@ set -e
 
 find simulated_tests/test_results/big/ -name '*.fa' | while read i; do
     echo "../wavy_choose.py -w 30 -m 10 $i | mawk 'NR%2==0{print(length($1))}' | sort | uniq -c > `dirname $i`/`basename $i .fa`.out"
-done #| \
-#parallel -j 4 {}
+done | \
+parallel -j 4 {}
+
+find simulated_tests/test_results/big/ -name '*clustered.fa.gz'
