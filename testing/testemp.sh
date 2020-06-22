53bfb1877a02a9e76a6f29eb72b89fcd634d9a2c
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-06-22 11:31:34 -0600

    fixed emp test to match actual run

--- testing/testemp.sh
+++ testing/testemp.sh
@@ -1,11 +1,10 @@
-cd empdir
-#
-#ls *_clustered.fa.gz | while read i; do
-#    echo "../choose_cwt.py -w 500 -m 200 <(gunzip -c $i) | pigz -p 7 > `basename $i _clustered.fa.gz`_chosen.fa.gz"
-#done | \
-#parallel -j 4 {}
+#!/bin/bash
+set -e
 
-ls | grep '_cluster_[0-9]*.fa.gz' | while read i; do
-    echo "../choose_cwt.py -w 500 -m 200 <(gunzip -c $i) | pigz -p 7 > `basename $i _clustered.fa.gz`_chosen.fa.gz"
+cd empirical_tests/full
+
+ls *.gz | while read i; do
+    echo "../../../wavy_choose.py -w 500 -m 200 -t 1 <(gunzip -c $i) | \
+    pigz -p 7 > test_results/`basename $i _clustered.fa.gz`_chosen.fa.gz"
 done | \
 parallel -j 4 {}
